3e041050e0c387ae49bd14078b1f17e15d7e990d
Kaname <kaname.aibyo@gmail.com>
2017-09-22 14:36:58 +0300

    HW 14

--- HW14-threads/src/test/java/ru/otus/kushchenko/threads/sorter/ParallelSorterTest.java
+++ HW14-threads/src/test/java/ru/otus/kushchenko/threads/sorter/ParallelSorterTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
@@ -11,30 +13,23 @@ import java.util.stream.Stream;
 
 class ParallelSorterTest {
     private final int N = 71;
-    private List<Integer> array;
+    private List<Integer> actual;
+    private List<Integer> expected;
 
 
     @BeforeEach
     void setUp() {
-        array = Stream.generate(() -> ThreadLocalRandom.current().nextInt(-100, 101))
+        actual = Stream.generate(() -> ThreadLocalRandom.current().nextInt(-100, 101))
                 .limit(N)
                 .collect(Collectors.toList());
+        expected = new ArrayList<>(actual);
     }
 
     @Test
     void sort() {
+        Collections.sort(expected);
         ParallelSorter sorter = new ParallelSorter();
-        Assertions.assertTrue(isSorted(sorter.sort(array, 4)));
+
+        Assertions.assertArrayEquals(expected.toArray(), sorter.sort(actual, 4).toArray());
     }
-
-    private <T extends Comparable<T>> boolean isSorted(List<T> list) {
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i - 1).compareTo(list.get(i)) > 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
 }
